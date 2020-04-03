949fa8fcee61e497f53d0b497b0a6c5eeaf54206
luke <lukesosbourne@gmail.com>
2020-04-03 13:46:25 +0100

    first commit

---
+++ src/main/java/com/qa/testdrivendevelopment/Runner.java
@@ -0,0 +1,40 @@
+package com.qa.testdrivendevelopment;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		String q1 = "hey there Z   hoho";
+//		char q1char = 'x';
+//
+//		String q2a = "bill";
+//		String q2b = "jill";
+//
+		String[] q3sa = { "Dog", "cat", "mat" };
+
+//		String q4s = "hEelLoooO";
+
+//		int q5i = 8;
+
+//		.....................String s1 = "bigword";
+//		.......................String s2 = "big";
+
+//
+		Questions q = new Questions();
+
+//		System.out.println(q.returnPosition(q1, q1char));
+
+//		System.out.println(q.zipped(q2a, q2b));
+
+		System.out.println(q.removeDuplicates(q3sa));
+
+//		System.out.println(q.getVowel(q4s));
+
+//		System.out.println(q.factorial(q5i));
+
+//		.............................System.out.println(q.wordFinder(s1, s2));
+
+	}
+
+}
